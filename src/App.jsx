56f24c70f7f0e14f29daa25d1957b6dd9d9a28cb
dafinysouza <22901057+dafinysouza@users.noreply.github.com>
2022-06-21 01:17:43 -0300

    feat: iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <>
+      <Post 
+        author="Dafiny" 
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo vero ducimus dicta aliquid eum reprehenderit perspiciatis eius eligendi fuga exercitationem eos nesciunt quas earum, voluptate non doloremque facilis? Pariatur, distinctio?"
+      />
+    </>
+  )
+}
